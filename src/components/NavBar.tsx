c5428330d2d018e51bea4edc299a32e9a064bfab
ESTEBAN QUINTERO <78872778+jesteban39@users.noreply.github.com>
2021-10-22 18:32:33 -0500

    dev6

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import Home from './../../../Pruebas/impormedica/src/components/Home';
 
 const NavBar = () => {
   return <div>
